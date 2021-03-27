b392adc6c5b56f4fb629f3ced2a7175e73d0ca34
YashDjsonDookun <dookun88@gmail.com>
2021-03-27 09:46:57 +0400

    Changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main{
 	public static void main(String[] args){
 		ConnectDatabase Db = new ConnectDatabase();
 		Db.DB_Connect();
-		// Patient patient = new Patient();
-		// patient.RegisterPatient();
+		Patient patient = new Patient();
+		patient.RegisterPatient();
 	}
 }
